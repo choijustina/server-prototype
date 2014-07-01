b81f448dd131c1a29e995ede5bd20840ac5ae959
Justina Choi <choi.justina@gmail.com>
2014-07-01 15:06:21 -0400

    adding rabbitmq version

--- src/sse/SseServer.java
+++ src/sse/SseServer.java
@@ -15,28 +15,35 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * File: SseServer.java
  * Project: SSE
+ * To access: http://localhost:8080/SSE/
  * Source: http://milestonenext.blogspot.com/2013/07/html5-server-sent-events-sample-with.html
  * Documentation: http://docs.oracle.com/javaee/6/api/javax/servlet/http/package-summary.html
  * 
  */
+
 @WebServlet("/SseServer")
 public class SseServer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doPost(request, response);
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/event-stream;charset=UTF-8");
 		response.setHeader("Cache-Control", "no-cache");
 		response.setHeader("Connection", "keep-alive");
-
+		
 		PrintWriter out = response.getWriter();
+		out.print("data: " + "success in Server.java file\n\n");
+		out.flush();
+		
 		String filename = "longfile_10.txt";
 		URL url = new URL("http://localhost:8080/SSE/" + filename);
 		
-		out.print("data: " + "Searching in " + filename + "\n\n");
+		out.print("data: " + "searching in " + filename + "\n\n");
 		out.flush();
 		
 		Scanner s = new Scanner(url.openStream());
@@ -55,9 +62,7 @@ public class SseServer extends HttpServlet {
 			
 			Date date = new Date();
 			out.print("event: " + event + "\n");
-			out.print("data: " + id + "  " + event 
-					+ " - " + msg + " accessed at " + date
-					+ "\n\n");
+			out.print("data: " + id + "  " + event + " - " + msg + " accessed at " + date + "\n\n");
 			out.flush();
 			
 			try {
