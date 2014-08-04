2e16c07097b5419481a3e41cd23aafc5dba1e05b
Justina Choi <choi.justina@gmail.com>
2014-08-04 16:20:21 -0400

    implemented AJAX tutorial, Retrieve the contents of an ASP file, in index.html

--- WebContent/CalendarPopup.js
+++ WebContent/CalendarPopup.js
@@ -411,7 +411,7 @@ function CP_select(inputobj, linkname, format) {
 	this.currentDate=null;
 	var time=0;
 	if (selectedDate!=null) {
-		time = getDateFromFormat(selectedDate,format)
+		time = getDateFromFormat(selectedDate,format);
 		}
 	else if (inputobj.value!="") {
 		time = getDateFromFormat(inputobj.value,format);
