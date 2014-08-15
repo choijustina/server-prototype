24d110ea367ce732beb0b1aaff08e68052306d16
Justina Choi <choi.justina@gmail.com>
2014-08-15 15:31:40 -0400

    mostly does filtering, except when more than one window open does round-robin

--- src/sse/RoutingConsumer.java
+++ src/sse/RoutingConsumer.java
@@ -32,9 +32,12 @@ import org.json.simple.parser.ParseException;
 public class RoutingConsumer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final boolean MSG_ACK = false;			// message acknowledgment off when true; receipts of messages are sent back from consumer telling okay to delete
-	//private static final String BINDING_KEY = "json";			
-	protected static final int RECONNECT_TIME = 10000;		// delay in milliseconds until auto-reconnect 
-	private static final String REDIRECT_FILE = "newSSE.html";
+	protected static final int RECONNECT_TIME = 3000;		// delay in milliseconds until auto-reconnect 
+	private static final String REDIRECT_FILE = "index.html";
+	
+	private static Connection connection = null;  //only one RabbitMQ connection
+	private static Channel channel = null;
+	private static QueueingConsumer queueingConsumer = null;
 	
 	private static Map<Integer, Consumer> consumerMap = new HashMap<Integer, Consumer>();
 	private static int numberOfConsumers = 0;
@@ -51,7 +54,7 @@ public class RoutingConsumer extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String str = request.getParameter("searchName");
+		String str = request.getParameter("name");
 		if (str==null)
 			isEventStream(request, response);
 		else {
@@ -68,37 +71,12 @@ public class RoutingConsumer extends HttpServlet {
 	 */
 	protected void isHTML(HttpServletRequest request, HttpServletResponse response) 
 			throws IOException, InterruptedException, ServletException {
-		/*
-		response.setContentType("text/html;charset=UTF-8");
-		response.setHeader("Cache-Control", "no-cache");
-		response.setHeader("Connection", "keep-alive");
+
+		String name = request.getParameter("name");
 		
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Search Results</title>");
-		out.println("<script type=\"text/javascript\">");
-		out.println("</script>");
-		out.println("</head>");
+		// TODO: if request.getParam returns null, then don't search for it
+		// TODO: add getParamater for other filter criteria
 		
-		out.println("<body>");
-		out.println("<h1>RoutingConsumer.java</h1><br>");
-		
-		String str = request.getParameter("searchName");
-		out.println("<p>searchName is: " + str + "</p><br>");
-		
-		out.println("<p>Should redirect after " + RECONNECT_TIME + " milliseconds</p><br>");
-		
-		out.println("</body>");
-		out.println("</html>");
-		out.flush();
-		out.close();
-		*/
-//		Thread.currentThread().sleep(RECONNECT_TIME);
-		
-		
-		String name = request.getParameter("searchName");
 		Consumer c = new Consumer(name);
 		
 		if (numberOfConsumers==0)
@@ -111,11 +89,6 @@ public class RoutingConsumer extends HttpServlet {
 		RequestDispatcher view = request.getRequestDispatcher(REDIRECT_FILE);
 		view.forward(request, response);
 		
-		
-//		out.println("</body>");
-//		out.println("</html>");
-//		out.flush();
-//		out.close();
 	}
 	
 	/*
@@ -126,83 +99,91 @@ public class RoutingConsumer extends HttpServlet {
 		response.setContentType("text/event-stream;charset=UTF-8");
 		response.setHeader("Cache-Control", "no-cache");
 		response.setHeader("Connection", "keep-alive");
-		/*
-		if (initialized==0) {
-			initialized = 1;
-			connection = initConnection(request, response);
-			channel = initChannel(connection);
-		}*/
-		
-		// RABBITMQ
-		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
-		Connection connection = factory.newConnection();
-		Channel channel = connection.createChannel();
-		channel.exchangeDeclare(AbstractProducer.EXCHANGE_NAME, AbstractProducer.EXCHANGE_TYPE);
-		channel.queueDeclare(AbstractProducer.QUEUE_NAME, true, false, false, null);
-		channel.queueBind(AbstractProducer.QUEUE_NAME, AbstractProducer.EXCHANGE_NAME, "");
 		
 		PrintWriter out = response.getWriter();
 		out.print("retry: " + RECONNECT_TIME + "\n\n");
-		/*
+
 		out.print("event: consumerKey\n");
-		out.print("data: " + consumerKey + "\n\n");
+		out.print("data: " + numberOfConsumers + "\n\n");
 		
-		out.print("event: " + consumerKey + "\n");
-		out.print("data: to confirm that consumerKey works as an event name\n\n");
-		*/
-		
-		/*
-		String str = request.getParameter("searchName");
-		if (str==null)
-			out.print("data: (Java) searchName parameter doesn't exist\n\n");
+		if (connection==null)
+			initRabbitMQ();
 		else
-			out.print("data: (Java) using request.getParameter to get searchName - " + str + "\n\n");
-		*/
+			out.print("data: RabbitMQ already initialized - double check\n\n");
 		
 		out.print("data: [*] Waiting for messages\n\n");
 		
 		out.print("data: numberOfConsumers counter: " + numberOfConsumers + "\n\n");
 		out.print("data: size of consumerMap: " + consumerMap.size() + "\n\n");
-		
+
+		/*
 		for (int i = 1; i <= numberOfConsumers; i++) {
 			//out.print("data: consumer " + i + ": " + consumerMap.get(i) + "\n\n");
 			Consumer consumer = consumerMap.get(i);
 			String n = consumer.getName();
 			out.print("data: consumer " + i + " is searching for: " + n + "\n\n");
 		}
-		
+		*/
+		Consumer consumer = consumerMap.get(numberOfConsumers);
+		String n = consumer.getName();
+		out.print("data: consumer " + numberOfConsumers + " is searching for " + n + "\n\n");
 		out.flush();
-		
-		QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
-		channel.basicConsume(AbstractProducer.QUEUE_NAME, MSG_ACK, queueingConsumer);
 		
 		while (true) {
 			try {
 				QueueingConsumer.Delivery delivery = queueingConsumer.nextDelivery();
 				
-				String routingKey = delivery.getEnvelope().getRoutingKey();
+				//String routingKey = delivery.getEnvelope().getRoutingKey();
 				String message = new String(delivery.getBody());
 				
 				if (message.equals(AbstractProducer.CLOSE_CONSUMER))
+					break;			
+		
+				// assumes is a json object
+				out.print("event: jsonobject\n");
+				
+				JSONParser parser = new JSONParser();
+				JSONObject jsonObject = (JSONObject) parser.parse(message);
+								
+				String eventName = (String) jsonObject.get("name");
+//				String eventID = (String) jsonObject.get("id");
+//				String eventMsgType = (String) jsonObject.get("msgtype");
+//				String eventDocType = (String) jsonObject.get("doctype");
+//				String eventData = (String) jsonObject.get("data");
+				
+				// check if the event matches any of the filter criteria
+				for (int i = 1; i <= numberOfConsumers; i++) {
+					Consumer c = consumerMap.get(i);
+					String s = c.getName();
+					
+					if (s.equals(eventName)) {
+						jsonObject.put("consumerKey", i);
+						out.print("data: " + jsonObject + "\n\n");
+					}
+
+					out.flush();
+				}
+				
+				
+				/*
+				if (message.equals(AbstractProducer.CLOSE_CONSUMER))
 					break;
 				else {
-					
 					if (message.equals("clear")) {
 						out.print("event: clear\n");
 						out.print("data: clearing the client display\n\n");
 					} else if (routingKey.equals("json")){
 						out.print("event: jsonobject\n");
 						out.print("data: " + message + "\n\n");
-						/*
-						JSONParser parser = new JSONParser();
-						JSONObject jsonObject = (JSONObject) parser.parse(message);
-						String eventName = (String) jsonObject.get("name");
-						String eventID = (String) jsonObject.get("id");
-						String eventMsgType = (String) jsonObject.get("msgtype");
-						String eventDocType = (String) jsonObject.get("doctype");
-						String eventData = (String) jsonObject.get("data");
-						*/
+						
+//						JSONParser parser = new JSONParser();
+//						JSONObject jsonObject = (JSONObject) parser.parse(message);
+//						String eventName = (String) jsonObject.get("name");
+//						String eventID = (String) jsonObject.get("id");
+//						String eventMsgType = (String) jsonObject.get("msgtype");
+//						String eventDocType = (String) jsonObject.get("doctype");
+//						String eventData = (String) jsonObject.get("data");
+						
 						//out.print("data: name is " + eventName + "\n\n");
 					} else {
 						out.print("data: " + routingKey + " - " + message + "\n\n");
@@ -210,18 +191,38 @@ public class RoutingConsumer extends HttpServlet {
 					//out.print("data: " + message + "\n\n");
 					out.flush();
 				}
-				
+				*/
 				channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
-			} /*catch (ParseException e) {
+			} catch (ParseException e) {
 				e.printStackTrace();
-			}*/
+			}
 		}
+		out.close();
+		closeRabbitMQ();
+	}
+	
+	/*
+	 * initRabbitMQ - initializes only one RabbitMQ connection
+	 */
+	private void initRabbitMQ() throws IOException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		connection = factory.newConnection();
+		channel = connection.createChannel();
+		
+		channel.exchangeDeclare(AbstractProducer.EXCHANGE_NAME, AbstractProducer.EXCHANGE_TYPE);
+		channel.queueDeclare(AbstractProducer.QUEUE_NAME, true, false, false, null);
+		channel.queueBind(AbstractProducer.QUEUE_NAME, AbstractProducer.EXCHANGE_NAME, "");
+		
+		queueingConsumer = new QueueingConsumer(channel);
+		channel.basicConsume(AbstractProducer.QUEUE_NAME, MSG_ACK, queueingConsumer);
+	}
+	
+	private void closeRabbitMQ() throws IOException {
 		connection.close();
 		channel.close();
-		out.close();
-		
 	}
 
 }
